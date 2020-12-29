6c548d06b9722ad33c64164dfecf31ca5f1b2768
Jörn Ruhlender <j.ruhlender@netz98.de>
2020-12-29 15:46:05 +0100

    Update rule matching
    - change html output

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.use(express.static('content'));
 
-app.get('/', (req, res) => {
-  res.status(404).sendFile('index.html');
-});
-
 app.get('/healthz', (req, res) => {
   res.send('Healthy!').end();
+});
+
+app.get('*', (req, res) => {
+  res.status(404).sendFile('index.html');
 });
 
 app.listen(8080, () => console.log('App listening on port 8080'));
